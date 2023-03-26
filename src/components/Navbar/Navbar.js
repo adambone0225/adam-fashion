5d54c10fe74c2b6018b358d2a7da5f7f18d39ef8
DESKTOP-LOAAV4F\USER <cheungkachun24@gmail.com>
2023-03-26 14:52:56 +0800

    navbar jsx set

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="wrapper">
+        <section className="left">
+          <div className="item">
+            <img
+              src="/image/pin.png"
+              alt=""
+              style={{ width: "2vw", height: "60%", objectFit: "cover" }}
+            />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1"> women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/1"> men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/1"> children</Link>
+          </div>
+          <SearchOutlinedIcon />
+        </section>
+        <section className="center">
+          <h1>AdamFashion</h1>
+        </section>
+        <section className="right">
+          <Link className="navBar-right" to="">
+            Homepage
+          </Link>
+          <Link className="navBar-right" to="">
+            About
+          </Link>
+          <Link className="navBar-right">Contact</Link>
+          <Link className="navBar-right">Stores</Link>
+          <div className="icons">
+            <LoginOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </section>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
