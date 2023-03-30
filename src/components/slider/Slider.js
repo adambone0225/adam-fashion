1e69d1425fddee94445f7322ddb1e79b2988d71e
DESKTOP-LOAAV4F\USER <cheungkachun24@gmail.com>
2023-03-30 21:10:12 +0800

    slider dots done

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Dot from "./Dot";
+
 import { useState, useRef, useEffect } from "react";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -11,7 +11,6 @@ let image = [slide1, slide2, slide3];
 
 const Slider = () => {
   const timeRef = useRef(null);
-  const dotRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const goToPrevious = () => {
     const newIndex = currentIndex === 0 ? image.length - 1 : currentIndex - 1;
@@ -64,11 +63,14 @@ const Slider = () => {
         <div className="panel">
           {image.map((_, index) => {
             return (
-              <Dot
+              <span
+                className={
+                  currentIndex == index ? "slider-dots active" : "slider-dots"
+                }
                 key={index}
-                index={index}
-                className="slider-dots"
-                slideSwitch={switchByDot}
+                onClick={(e) => {
+                  switchByDot(index);
+                }}
               />
             );
           })}
