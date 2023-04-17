01f69f05c0656ed0012c2a2f2b496294fd0e4edb
DESKTOP-LOAAV4F\USER <cheungkachun24@gmail.com>
2023-04-17 16:37:15 +0800

    cart finish

---
+++ src/components/Cart.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { data2 } from "./data";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+const Cart = () => {
+  console.log(data2);
+  return (
+    <div className="cart">
+      <h1>item in cart</h1>
+      {data2.map((item) => (
+        <div className="cart-item" key={item.id}>
+          <img src={item.coverImg} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.description?.substring(0, 100)}</p>
+            <div className="price"> 1 x ${item.price}</div>
+          </div>
+          <DeleteForeverIcon className="delete" />
+        </div>
+      ))}
+      <div className="total">
+        <span>SUBTOTOAL</span>
+        <span>$123</span>
+      </div>
+      <button>PROCEDD TO CHECKOUT</button>
+      <span className="reset">Reset Cart</span>
+    </div>
+  );
+};
+
+export default Cart;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "../components/Cart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import BalanceOutlinedIcon from "@mui/icons-material/BalanceOutlined";
 
 const Product = () => {
+  const [image, setImage] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+
+  const data = [
+    "https://img.freepik.com/free-photo/empty-drinking-glass-macro-shot_53876-33861.jpg?w=740&t=st=1681374246~exp=1681374846~hmac=3933184910c809297b5672f852662e408a139c179b64669aec48f99563894c2b",
+    "https://img.freepik.com/free-photo/medium-shot-smiley-man-with-trucker-hat_23-2149412793.jpg?w=1060&t=st=1681374334~exp=1681374934~hmac=418e74424b3ed42d0519742f2d72890a9885734e3703effa4a6d537e9dd5e7f1",
+  ];
   return (
-    <div>
+    <div className="product">
       <section className="left">
         <div className="gallery">
-          <img src="" alt="" />
+          <img src={data[0]} alt="" onClick={() => setImage(0)} />
+          <img src={data[1]} alt="" onClick={() => setImage(1)} />
+        </div>
+        <div className="display-image">
+          <img src={data[image]} alt="" />
+        </div>
+      </section>
+
+      <section className="right">
+        <h2>title</h2>
+        <span className="price">$200</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+          quos, eligendi doloremque blanditiis tempora repellat!
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((cur) => (cur === 1 ? 1 : cur - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((cur) => cur + 1)}>+</button>
+        </div>
+        <button className="add">
+          <ShoppingCartOutlinedIcon /> ADD TO CART
+        </button>
+        <div className="functions">
+          <button className="item">
+            <FavoriteBorderIcon /> WISH LIST
+          </button>
+          <button className="item">
+            <BalanceOutlinedIcon /> COMPARE
+          </button>
+        </div>
+        <div className="info">
+          <span>Producer: </span>
+          <span>Category</span>
+        </div>
+        <br />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>FAQ</span>
         </div>
       </section>
     </div>
