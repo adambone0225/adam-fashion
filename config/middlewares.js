22600f93a872c7d8fc4fb67789a115802c480bc9
DESKTOP-LOAAV4F\USER <cheungkachun24@gmail.com>
2023-05-22 19:12:07 +0800

    config correction

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,21 +1,7 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      headers: [
-        "accept-language",
-        "Token",
-        "Content-Type",
-        "Authorization",
-        "Accept",
-        "Origin",
-      ],
-      origin: ["http://localhost:1337/api/orders", "http://localhost:3000"],
-      keepHeaderOnError: true,
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
--- config/server.js
+++ config/server.js
@@ -4,18 +4,6 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  name: "strapi::cors",
-  config: {
-    headers: [
-      "accept-language",
-      "Token",
-      "Content-Type",
-      "Authorization",
-      "Accept",
-      "Origin",
-    ],
-    origin: ["http://localhost:1337/api/orders", "http://localhost:3000"],
-  },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
